e45ad3ed1f5093b2642eaf2ea720cbbf873cdb56
Sam Wong <sam.wong.uk@gmail.com>
2014-11-17 15:28:01 -0800

    added activity tab codes

---
+++ Activity.h
@@ -0,0 +1,16 @@
+//
+//  Activity.h
+//  I'm In
+//
+//  Created by Sam Wong on 17/11/2014.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Activity : NSObject
+
+- (instancetype) initWithDictionary: (NSDictionary *) activityDictionary;
+- (NSMutableArray *) parseJSONDataIntoActivities: (NSData *)rawJSONData;
+
+@end
