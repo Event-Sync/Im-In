171fedb8b9a41e3e3525a9fd6fc6cf1f149a0fba
mbrightbill <brightbill.mt@gmail.com>
2014-11-17 14:08:08 -0800

    Added layout and outlets for signup and login VCs

--- SignUpViewController.h
+++ SignUpViewController.h
@@ -10,4 +10,13 @@
 
 @interface SignUpViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UILabel *phoneNumberLabel;
+@property (weak, nonatomic) IBOutlet UITextField *phoneNumberTextField;
+@property (weak, nonatomic) IBOutlet UILabel *passwordLabel;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *registerButton;
+
 @end
