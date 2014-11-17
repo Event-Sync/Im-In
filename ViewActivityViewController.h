82a39a29c3589d61ef0d27460425c04d8b8523cf
mbrightbill <brightbill.mt@gmail.com>
2014-11-17 12:36:26 -0800

    Added backbone of project

---
+++ ViewActivityViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewActivityViewController.h
+//  I'm In
+//
+//  Created by Matthew Brightbill on 11/17/14.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewActivityViewController : UIViewController
+
+@end
