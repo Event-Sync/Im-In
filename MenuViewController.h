c72776f6d4e206ccfe84a85dcf1328e9138bcd2c
mbrightbill <brightbill.mt@gmail.com>
2014-11-17 12:10:34 -0800

    Added progress on menu

---
+++ MenuViewController.h
@@ -0,0 +1,17 @@
+//
+//  MenuViewController.h
+//  I'm In
+//
+//  Created by Matthew Brightbill on 11/17/14.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UIButton *signUpButton;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+
+@end
---
+++ MenuViewController.m
@@ -0,0 +1,29 @@
+//
+//  MenuViewController.m
+//  I'm In
+//
+//  Created by Matthew Brightbill on 11/17/14.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+#import "MenuViewController.h"
+
+@interface MenuViewController ()
+
+
+
+@end
+
+@implementation MenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    [[self.signUpButton layer] setBorderWidth:2.0f];
+    [[self.signUpButton layer] setBorderColor:[UIColor whiteColor].CGColor];
+    
+    
+}
+
+
+@end
